d4a006b8baac9ac76bc8c58aeb9c79d938ed31b4
Fran-B-Git <f.bacic@stroeermediabrands.de>
2024-06-05 10:21:25 +0200

    startup update

--- startup.sh
+++ startup.sh
@@ -1,1 +1,10 @@
+source ~/Scripts/source.sh
+git checkout -b backup
 cp ~/.config/i3/config ~/Scripts/i3/config
+git add ~/Scripts/i3/config
+git add ~/Scritpts/startup.sh
+git commit -m "i3 backup"
+gitup
+git checkout master
+git merge backup
+git -D backup
