7064fe4563b2a749f8a95adc77453cc284196f2e
Fran-B-Git <f.bacic@stroeermediabrands.de>
2024-10-10 17:29:37 +0200

    update source for new name of shared connection

--- source.sh
+++ source.sh
@@ -23,7 +23,7 @@ alias left-of="xrandr --output HDMI-1 --auto --left-of eDP-1"
 alias right-of="xrandr --output HDMI-1 --auto --right-of eDP-1"
 
 alias mobup="nmcli c up \"goofytime\""
-alias deskup="nmcli c up \"New 802-3-ethernet connection\""
+alias deskup="nmcli c up \"share_internet\""
 alias hn49="ssh hn49"
 alias repo="~/Scripts/repo.sh"
 alias elastic="~/Scripts/elastic.sh"
