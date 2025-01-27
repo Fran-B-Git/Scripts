624fba89702d5747d63f6ce67b59516e6b348f06
Fran-B-Git <f.bacic@stroeermediabrands.de>
2025-01-27 13:41:16 +0100

    make bg

--- fancy.sh
+++ fancy.sh
@@ -3,7 +3,7 @@
 clear
 printf -- '=%0.s' {1..95}
 echo ""
-curl -s -L https://vrijeme.hr/zggric.jpg --output - | ascii-image-converter - -b --dither -C -W 95
+curl -s -L https://vrijeme.hr/zggric.jpg --output - | ascii-image-converter - -C --color-bg -m "  " -W 95
 printf -- '=%0.s' {1..95}
 echo ""
 echo -e '\033[0;32m'
