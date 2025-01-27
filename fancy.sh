90c835eb659ce1ddae107f38aa9b3580fbbe1c00
Fran-B-Git <f.bacic@stroeermediabrands.de>
2025-01-27 13:56:36 +0100

    fix numbering

--- fancy.sh
+++ fancy.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 clear
-printf -- '=%0.s' {1..95}
+D=${1-${BAR-95}}
+printf -- '=%0.s' `seq $D`
 echo ""
-curl -s -L https://vrijeme.hr/zggric.jpg --output - | ascii-image-converter - -C --color-bg -m "  " -W 95
-printf -- '=%0.s' {1..95}
+curl -s -L https://vrijeme.hr/zggric.jpg --output - | ascii-image-converter - -C --color-bg -m "  " -W $D
+printf -- '=%0.s' `seq $D`
 echo ""
 echo -e '\033[0;32m'
 /usr/share/i3blocks/wttrin
