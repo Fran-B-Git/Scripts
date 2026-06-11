d93908a88eef73c3ffeb2dcaf9e18509bb480519
Fran-B-Git <fran.bacic@gmail.com>
2026-06-11 10:15:29 +0200

    fix

--- sh/minihost.sh
+++ sh/minihost.sh
@@ -1,11 +1,9 @@
 if [ $# -eq 0 ];then
-	ip=`ip addr show wlp99s0 | grep "inet " | grep -oP "([0-9].)*" | grep -o "^.*/"`
-	ip=${ip::-1}
-
+	ip=`ip addr show wlp99s0 | grep "inet " | grep -oP "([0-9].)*" | head -n 1`
 else
 	ip=$1
 fi
 ip=http://$ip:8000
-echo $ip
 qrencode -o - -t ANSI "$ip"
+echo $ip
 python3 -m http.server 
